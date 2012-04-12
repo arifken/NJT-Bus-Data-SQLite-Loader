ba4d071e5a51dda4b47840c7a51ce354413586df
Andy Rifken <whatsup@andyrifken.com>
2012-04-12 08:04:39 -0400

    initial commit

---
+++ BusDataLoader/main.cpp
@@ -0,0 +1,195 @@
+/*
+schema:
+
+calendar_dates
+--------------
+service_id      integer (pk)
+date            integer (unix timestamp)
+exception_type  integer
+
+
+routes
+-------
+route_id            integer (pk)
+agency_id           integer (join agency table)
+route_short_name    text
+route_long_name     text
+route_type          integer
+route_url           text
+route_color         text?
+
+stop_times
+----------
+trip_id             integer (join trips)
+arrival_time        text (HH:MM:SS)
+departure_time      text (HH:MM:SS)
+stop_id             integer (join stops table)
+stop_sequence       integer
+pickup_type         integer
+drop_off_type       integer
+shape_dist_traveled real
+
+stops
+------
+stop_id             integer (pk)
+stop_code           integer
+stop_name           text
+stop_desc           text
+stop_lat            real
+stop_lon            real
+zone_id             integer
+
+trips
+------
+trip_id             integer (pk)
+route_id            integer
+service_id          integer
+trip_headsign       text
+direction_id        integer
+block_id            text
+shape_id            integer
+
+
+agencies
+-------
+agency_id           integer (pk)
+agency_name         text
+agency_url          text
+agency_timezone     text
+agency_lang         text
+agency_phone        text
+
+
+shapes
+------
+shape_id                integer (pk)
+shape_pt_lat            real
+shape_pt_lon            real
+shape_pt_sequence       integer
+shape_dist_traveled     real
+
+
+ */
+
+#include <iostream>
+#include <sqlite3.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <vector>
+
+
+const char *fn_calendarDates = "calendar_dates.txt";
+const char *fn_routes = "routes.txt";
+const char *fn_stopTimes = "stop_times.txt";
+const char *fn_stops = "stops.txt";
+const char *fn_trips = "trips.txt";
+const char *fn_shapes = "shapes.txt";
+const char *fn_agency = "agency.txt";
+
+
+void usage() {
+    std::cout << "\nUsage: $ ./loader [path to bus data directory] [output sqlite directory]\n";
+}
+
+int create_database(const char *path, sqlite3 **retDb) {
+    printf("\ncreating database...");
+    sqlite3 *db = NULL;
+    int ok = 0;
+
+    ok = sqlite3_open(path, &db);
+    if (ok == SQLITE_OK) {
+        sqlite3_stmt *stmt;
+        const char *pzTail;
+
+        int numTables = 7;
+        char const *sql[] = {
+                "CREATE TABLE agencies (agency_id INTEGER PRIMARY KEY, agency_name TEXT, agency_url TEXT, agency_timezone TEXT, agency_lang TEXT, agency_phone TEXT)",
+
+                "CREATE TABLE calendar_dates (id INTEGER PRIMARY KEY, service_id INTEGER, date INTEGER, exception_type INTEGER)",
+
+                "CREATE TABLE routes (route_id INTEGER PRIMARY KEY, agency_id INTEGER, rout_short_name TEXT, route_long_name TEXT, route_type INTEGER, route_url TEXT, route_color TEXT)",
+
+                "CREATE TABLE stop_times (id INTEGER PRIMARY KEY, trip_id INTEGER, arrival_time TEXT, departure_time TEXT, stop_id INTEGER, stop_sequence INTEGER, pickup_type INTEGER, drop_off_type INTEGER, shape_dist_traveled REAL)",
+
+                "CREATE TABLE stops (stop_id INTEGER PRIMARY KEY, stop_code INTEGER, stop_name TEXT, stop_desc TEXT, stop_lat REAL, stop_lon REAL, zone_id INTEGER)",
+
+                "CREATE TABLE trips (trip_id INTEGER PRIMARY KEY, route_id INTEGER, service_id INTEGER, trip_headsign TEXT, direction_id INTEGER, block_id TEXT, shape_id INTEGER)",
+
+                "CREATE TABLE shapes (shape_id INTEGER PRIMARY KEY, shape_pt_lat REAL, shape_pt_lon REAL, shape_pt_sequence INTEGER, shape_dist_traveled REAL)",
+        };
+
+        printf("\nnum statments: %i", numTables);
+        for (int i = 0; i < numTables; i++) {
+            sqlite3_prepare_v2(db, sql[i], strlen(sql[i]), &stmt, &pzTail);
+            sqlite3_step(stmt);
+            sqlite3_free(stmt), stmt = NULL;
+        }
+
+    }
+
+    sqlite3_close(db);
+
+    if (retDb != NULL) {
+        *retDb = db;
+    }
+}
+
+int load_calendar_dates(const char *dir_path) {
+
+}
+
+int load_routes(const char *path) {
+
+}
+
+int load_stop_times(const char *path) {
+
+}
+
+int load_stops(const char *path) {
+
+}
+
+int load_trips(const char *path) {
+
+}
+
+int load_agencies(const char *path) {
+
+}
+
+int load_shapes(const char *path) {
+
+}
+
+
+
+int load_data(const char *dir_path, const char *db_path) {
+    std::string joined = std::string(dir_path);
+    joined.append("/").append(fn_calendarDates);
+
+    printf("\npath: %s", joined.c_str());
+
+
+}
+
+int main(int argc, const char *argv[]) {
+
+    if (argc != 3) {
+        usage();
+        return 0;
+    }
+
+    const char *dir_path = argv[1];
+    const char *db_path = argv[2];
+
+    sqlite3 *db;
+    create_database(db_path, &db);
+    load_data(dir_path, db_path);
+
+    return 0;
+}
+
+
+
+
